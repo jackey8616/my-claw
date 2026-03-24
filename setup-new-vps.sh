220c488de6de326830631498ba03a1ef5e3f768c
Clooooode <clode@clo5de.info>
2026-03-24 16:31:55 +0800

    fix: folders pre-create

--- setup-new-vps.sh
+++ setup-new-vps.sh
@@ -1,3 +1,10 @@
 sudo apt update && sudo apt upgrade -y
 curl -fsSL https://get.docker.com | sh
 sudo usermod -aG docker $USER
+
+mkdir -p openclaw/data
+mkdir -p openclaw/skills
+mkdir -p caddy/data
+mkdir -p caddy/config
+sudo chmod -R 775 openclaw && sudo chown -R 1000:1000 openclaw
+sudo chmod -R 775 caddy && sudo chown -R 1000:1000 caddy
