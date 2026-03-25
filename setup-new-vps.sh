d4bec516ebc37477b91fec97fb36860b845ed736
Clooooode <clode@clo5de.info>
2026-03-25 13:48:02 +0800

    feat: add obsidian vault sync mechanism

--- setup-new-vps.sh
+++ setup-new-vps.sh
@@ -1,9 +1,56 @@
+#!/bin/bash
+REMOTE_DEVICE_ID=""
+VAULT_ID=""
+read -p "REMOTE_DEVICE_ID: " REMOTE_DEVICE_ID
+read -p "VAULT_ID: " VAULT_ID
+
 sudo apt update && sudo apt upgrade -y
 curl -fsSL https://get.docker.com | sh
 sudo usermod -aG docker $USER
 
-mkdir -p openclaw/data
 mkdir -p openclaw/obsidian-vault
+docker compose up -d openclaw-obsidian
+sleep 8
+
+SYNCTHING_CONFIG_FILE="./syncthing/config/config.xml"
+if [ -f "$SYNCTHING_CONFIG_FILE" ]; then
+  cp "$SYNCTHING_CONFIG_FILE" "$SYNCTHING_CONFIG_FILE.bak"
+  sed -i 's|<listenAddress>tcp://default</listenAddress>|<listenAddress>tcp://0.0.0.0</listenAddress>|g' "$SYNCTHING_CONFIG_FILE"
+  API_KEY=$(sed -n 's:.*<apikey>\(.*\)</apikey>.*:\1:p' "$SYNCTHING_CONFIG_FILE" | head -1)
+else
+  echo "Error: missing $SYNCTHING_CONFIG_FILE"
+  exit 1
+fi
+
+DEVICE_ID=$(docker exec openclaw-obsidian curl -s \
+  -H "X-Api-Key: $API_KEY" http://127.0.0.1:8384/rest/system/status | jq -r '.myID')
+
+docker exec openclaw-obsidian curl -s -X POST \
+  -H "X-API-Key: $API_KEY" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"deviceID\": \"$REMOTE_DEVICE_ID\",
+    \"name\": \"MacBookAir\",
+    \"autoAcceptFolder\":true
+  }" \
+  http://127.0.0.1:8384/rest/config/devices
+
+
+docker exec openclaw-obsidian curl -s -X POST \
+  -H "X-API-Key: $API_KEY" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"id\":\"$VAULT_ID\",
+    \"label\":\"Obsidian Vault\",
+    \"path\":\"/data/obsidian_vault\",
+    \"type\":\"sendreceive\",
+    \"devices\":[
+      {\"deviceID\":\"$REMOTE_DEVICE_ID\"}
+    ]
+  }" \
+  http://127.0.0.1:8384/rest/config/folders
+
+mkdir -p openclaw/data
 mkdir -p openclaw/skills
 sudo chmod -R 775 openclaw && sudo chown -R 1000:1000 openclaw
 docker run -it --rm \
