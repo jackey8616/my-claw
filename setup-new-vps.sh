94a9399b7fb8e4ddf2f007041370170ca55fea37
Clooooode <clode@clo5de.info>
2026-03-27 22:50:55 +0800

    fix(script): missing gateway restart

--- setup-new-vps.sh
+++ setup-new-vps.sh
@@ -170,6 +170,8 @@ run_as_openclaw() {
     ./openclaw/data/openclaw.json > temp.json \
     && mv temp.json ./openclaw/data/openclaw.json
 
+  docker exec -ti openclaw-app openclaw gateway restart
+
   echo "    allowedOrigins added https://$REVERSE_PROXY_DOMAIN"
 
   # ============================================================
